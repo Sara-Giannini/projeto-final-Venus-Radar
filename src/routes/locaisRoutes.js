f65e6d32fb437c463e65015a878059c03a0bf9be
Sara <saragiannini593@gmail.com>
2022-07-25 20:53:17 -0300

    configura rotas de locais

---
+++ src/routes/locaisRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const router = express.Router()
+const controller = require("../controllers/locaisController")
+
+router.get("/locais", controller.getAll)
+router.get("/locais/:id", controller.getById)
+router.get("/estados", controller.getByState)
+router.get("/cidades", getByCity)
+router.post("/local", controller.addNew)
+router.patch("/local/update/:id", controller.updateById)
+router.delete("/local/delete/:id", controller.deleteById)
