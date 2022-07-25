1608c74d84053504141f472dea002681393caa5f
Sara <saragiannini593@gmail.com>
2022-07-25 20:52:32 -0300

    configura rotas de pessoa usuária

--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -2,9 +2,9 @@ const express = require("express")
 const router = express.Router()
 const controller = require("../controllers/usuariosController")
 
-router.post("/usuario", controller.create)
+router.post("/usuario", controller.newUser)
 router.post("/usuario/login", controller.login)
-router.patch("/usuario/update/:id", controller.updateById)
-router.delete("/usuario/delete/:id", controller.deleteById)
+router.patch("/usuario/update/:id", controller.updateUserById)
+router.delete("/usuario/delete/:id", controller.deleteUserById)
 
 module.exports = router
