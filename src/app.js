9c8a5cd7832a75bae99d0c0458fc79d63513b173
Sara <saragiannini593@gmail.com>
2022-07-25 20:26:45 -0300

    configura app

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('./database/mongooseConnect')
+const locaisRoutes = require('./routes/locaisRoutes')
+const usuariosRoutes = require('./routes/usuariosRoutes')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect()
+
+app.use(locaisRoutes)
+app.use(usuariosRoutes)
+
+module.exports = app
