42517639660207f00aff5f6a41569f050611eaeb
Sara <saragiannini593@gmail.com>
2022-07-25 21:47:53 -0300

    configura model de locais

---
+++ src/models/locaisModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const localSchema = mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: mongoose.Types.ObjectId
+    },
+    nome: { type: String, required: true },
+    estado: { type: String, required: true },
+    cidade: { type: String, required: true },
+    endereço: { type: String, required: true },
+    categoria: { type: String, required: true },
+    descricao: {String}
+},
+    { timestamps: true }
+)
+
+const locais = mongoose.model('locais', localSchema)
+module.exports = locais
